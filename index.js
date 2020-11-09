a6ba7aa0376dca8605cd09b34888e410a1a40007
Maverick Oleson <Moleso587@West-Mec.org>
2020-11-08 19:33:09 -0700

    finished js, in order to create basic gallery scroll, and made js and css external

---
+++ index.js
@@ -0,0 +1,38 @@
+var pic = [document.getElementById("img1"), document.getElementById("img2"), document.getElementById("img3")]
+pic[0].style.visibility = "visible";
+pic[1].style.visibility = "hidden";
+pic[2].style.visibility = "hidden";
+function back(){
+    if(pic[0].style.visibility == 'visible'){
+        pic[0].style.visibility = 'hidden';
+        pic[1].style.visibility = 'hidden';
+        pic[2].style.visibility = 'visible';
+    }
+    else if(pic[2].style.visibility == 'visible'){
+        pic[2].style.visibility = 'hidden';
+        pic[0].style.visibility = 'hidden';
+        pic[1].style.visibility = 'visible';
+    }
+    else if(pic[1].style.visibility == 'visible'){
+        pic[1].style.visibility = 'hidden';
+        pic[2].style.visibility = 'hidden';
+        pic[0].style.visibility = 'visible';
+    }
+}
+function forward(){
+    if(pic[0].style.visibility == 'visible'){
+        pic[0].style.visibility = 'hidden';
+        pic[2].style.visibility = 'hidden';
+        pic[1].style.visibility = 'visible';
+    }
+    else if(pic[2].style.visibility == 'visible'){
+        pic[2].style.visibility = 'hidden';
+        pic[1].style.visibility = 'hidden';
+        pic[0].style.visibility = 'visible';
+    }
+    else if(pic[1].style.visibility == 'visible'){
+        pic[1].style.visibility = 'hidden';
+        pic[0].style.visibility = 'hidden';
+        pic[2].style.visibility = 'visible';
+    }
+}
