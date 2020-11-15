c7565f4c9f56f99bf2d7c4b277d2af8cc8a585ce
Maverick Oleson <Moleso587@West-Mec.org>
2020-11-15 05:31:24 -0700

    got rid of accidental else statement

--- js/Trivia.js
+++ js/Trivia.js
@@ -8,7 +8,7 @@ var answer15 = document.getElementById("answer15");
 
 var answer18 = document.getElementById("answer18");
 
-var p = document.getElementsByTagName("P")[0];
+var p = document.getElementsByTagName("P")[1];
 p.style.visibility = "hidden";
 
 var score = "0";
@@ -16,9 +16,6 @@ var score = "0";
 function trivia(){
     if(answer4.checked){
         score++;
-    }
-    else{
-
     }
     if(answer5.checked){
         score++;
